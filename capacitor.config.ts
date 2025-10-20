c9a86c9ca6cd534298e3d5a3d4527f56f5295bd2
Haithem-SBOUI <haithem.sb999@gmail.com>
2025-10-20 15:17:11 +0200

    init project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'feeling-dhikr-app',
+  webDir: 'www'
+};
+
+export default config;

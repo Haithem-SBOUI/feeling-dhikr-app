82cfb0f8f5c9ed53330a3e29c8d14ae5e356780f
Haithem-SBOUI <haithem.sb999@gmail.com>
2025-10-21 16:12:44 +0200

    update Capacitor configuration and add Android platform support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'feeling-dhikr-app',
+  appId: 'com.haithemsboui.feelingdhikr',
+  appName: 'Feeling Dhikr',
   webDir: 'www'
 };
 

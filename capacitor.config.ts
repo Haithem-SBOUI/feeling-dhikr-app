c813c0e24d8b4d8b15f9e8dedd01ef59b77b1c48
Haithem-SBOUI <haithem.sb999@gmail.com>
2025-10-22 17:10:05 +0200

    enhance status bar configuration and add safe area padding for better UI responsiveness

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.haithemsboui.feelingdhikr',
   appName: 'Feeling Dhikr',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      style: 'dark',
+      backgroundColor: '#3880ff',
+      overlaysWebView: false
+    }
+  }
 };
 
 export default config;
